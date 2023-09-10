b19bc865a7ed9d29205cf698bac3339314be9457
Vinit Gohil <vgohil@me.com>
2023-09-10 14:59:31 +0100

    Updated web app. Re-aligned the input to be at the top of the list box and implemented new feature to clear session state of new task upon pressing enter.

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from modules import utils
+import time
 
 taskList = utils.read_taskfile()
 
@@ -7,12 +8,17 @@ def addTask():
     task = st.session_state['newTask'] + "\n"
     taskList.append(task.capitalize())
     utils.write_taskfile(taskList)
-
+    st.session_state["newTask"] = ""
 
 
 st.title("My Todo App")
 st.subheader("Web Application to add tasks to your to-do list")
-st.text("This is to manage your tasks and your productivity")
+st.write("This is to manage your tasks and your <b>productivity</b>.",
+        unsafe_allow_html=True)
+
+
+st.text_input(label="Enter a Task to do:", placeholder="Add task...",
+              on_change=addTask, key='newTask')
 
 for index, task in enumerate(taskList):
     checkboxTask = st.checkbox(task, key=task)
@@ -23,6 +29,3 @@ for index, task in enumerate(taskList):
         st._rerun()
 
 
-
-st.text_input(label="Enter a Task to do:", placeholder="Add task...",
-              on_change=addTask, key='newTask')
